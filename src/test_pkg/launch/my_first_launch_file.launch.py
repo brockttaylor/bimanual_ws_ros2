090dc88c2856ec4dd68934cd5001012d4b2b7205
James Vickery <jov2103@columbia.edu>
2024-10-29 12:58:38 -0400

    Created a test launch file and got it working needed to edit package.xml and setup.py

---
+++ src/test_pkg/launch/my_first_launch_file.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    turtlesim_node = Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim'
+        )
+    
+    ld.add_action(turtlesim_node)
+    return ld
