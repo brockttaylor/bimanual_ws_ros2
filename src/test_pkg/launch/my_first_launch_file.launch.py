cbb6644eb4e40b0e80fa5830f313fd19343c0238
James Vickery <jov2103@columbia.edu>
2024-10-29 18:57:56 -0400

    Added different versions of my_first_launch_file from the tutorial

--- src/test_pkg/launch/my_first_launch_file.launch.py
+++ src/test_pkg/launch/my_first_launch_file.launch.py
@@ -1,13 +1,73 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
+# for event handlers
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnExecutionComplete
+
+# for pathjoin example
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+#for Launch Conditions
+from launch.actions import LogInfo
+from launch.conditions import IfCondition
+from launch.substitutions import PythonExpression, EnvironmentVariable
+
+# Event handlers
+'''def generate_launch_description():
     ld = LaunchDescription()
-    turtlesim_node = Node(
+    turtlesim1 = Node(
             package='turtlesim',
             executable='turtlesim_node',
-            name='sim'
+            name='turtlesim1'
         )
     
-    ld.add_action(turtlesim_node)
-    return ld
+    turtlesim2 = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        name='turtlesim2'
+    )
+    # ld.add_action(turtlesim1_node)
+    # return ld
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnExecutionComplete(
+                target_action=turtlesim1,
+                on_completion=[turtlesim2]
+            )
+        ),
+        turtlesim1
+    ])'''
+
+#PathJoinSubstitution for constructing the path to a file
+    # needs to have ur_description package installed
+'''def generate_launch_description():
+    
+    rviz_config_file = PathJoinSubstitution(
+            [FindPackageShare("ur_description"), "rviz", "view_robot.rviz"]
+    )
+
+    return LaunchDescription([
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            output="log",
+            arguments=["-d", rviz_config_file]
+        ),
+    ])'''
+
+# Launch Conditions 
+    # (tests if environment variable is true and changes output)
+def generate_launch_description():
+    return LaunchDescription([
+        LogInfo(condition=IfCondition(
+            PythonExpression(["'",
+                EnvironmentVariable('USER'),
+                "' == 'ubuntu'"
+            ])
+        ),
+        msg='Welcome back James'
+        )
+    ])
